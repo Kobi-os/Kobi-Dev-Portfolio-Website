dd7a9e5e6a365d8e674f22a0a1db5f46e36ba8de
Wojtek <wojtek.kobier@wp.pl>
2024-02-29 22:04:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// Code: Main App component that renders all the components
+import React from 'react'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Skills  from './components/Skills'
+import Projects  from './components/Projects'
+import Contact  from './components/Contact'
+
+const App = () => {
+  return (
+  <div className="container">
+    <Navbar />
+    <About />
+    <Skills />
+    <Projects />
+    <Contact />
+  </div>
+  )
+}
+
+export default App
