25c05764661b805c461f88e8327450b59999cfaa
Wojtek <wojtek.kobier@wp.pl>
2024-03-12 20:02:03 +0100

    almost finished skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ import WebIcon from '@mui/icons-material/Web';
 import StorageIcon from '@mui/icons-material/Storage';
 import Typography from '@mui/material/Typography';
 import ContentPasteGoIcon from '@mui/icons-material/ContentPasteGo';
-import { motion } from 'framer-motion';
+import { motion, wrap } from 'framer-motion';
 import { useInView } from 'framer-motion'
 
 
@@ -64,29 +64,32 @@ const Skills = () => {
 
   return (
     <Container className='skillsContainer'  maxWidth='xl' sx={{margin:'100px 0 100px 0'}}>
-      <Grid container spacing={3} component={motion.div} ref={ref}  style={{ transform: isInView ? "none" : "translateY(200px)", opacity: isInView ? 1 : 0, transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+      <Grid container spacing={3} component={motion.div} ref={ref}  sx={{
+        flexWrap: 'wrap', 
+        transform: isInView ? "none" : "translateY(200px)", opacity: isInView ? 1 : 0, transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
         }}>
         {content.map((service) => (
-        <Grid item key={service} xs={12} sm={6} md={4} >
-          <Card className='card'>
+        <Grid item key={service}  xs={12} sm={6} md={4}  sx={{ display: 'flex', justifyContent: 'center' , alignItems: 'center', flexDirection: 'column',}}>
+          <Card className='card'  sx={{ height: 250,  boxSizing: 'border-box', width:'100%'}}>
             <div className='blob'></div>
-            <CardContent className='bg' sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-              <Icon className='icon'>
+            <CardContent  className='bg'  sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', wordWrap: 'break-word',}}>
+              <Icon sx={{}}>
                 {service.icon}
               </Icon>
-              <Typography color='white' gutterBottom variant="h5" component="div" align='center' sx={{fontSize: 25, fontWeight: 600, fontFamily: "Inter",}}>
+              <Typography color='white' gutterBottom variant="h5" component="div" align='center' sx={{fontSize: 25, fontWeight: 600, fontFamily: "Inter", wordWrap: 'break-word', maxWidth: '100%', margin: '10px 0' }}>
                 {service.title}
               </Typography>
-              <Typography variant="body2"  component="div" align='center' sx={{fontSize: 17, fontWeight: 600, fontFamily: "Inter"}}>
+              <Typography  align='center' sx={{fontSize: 17, fontWeight: 600, fontFamily: "Inter", wordWrap: 'break-word', maxWidth: '100%' }}>
                 {service.description}
               </Typography>
             </CardContent>
+            
           </Card>
         </Grid>
         ))}
       </Grid>
       <Typography color='white' align='center' fontSize={20} margin={10} display={'inline-block'}>
-        <h2>Tech Stack</h2>
+        <h2 style={{textDecoration: 'underline', textDecorationColor: 'red', }}>Tech Stack</h2>
           {skills.map((skill) => (
             <button className='button'>
               {skill.technology}
