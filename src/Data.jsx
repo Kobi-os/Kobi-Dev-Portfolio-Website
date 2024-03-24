84c3ee7285cc2836796bbc1b60d8a38ef3e8f0d6
Wojtek <wojtek.kobier@wp.pl>
2024-03-24 15:05:10 +0100

    projects

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const projects = [
+    {
+        title: "AutoXpert",
+        description: "lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet",
+        src: "/src/assets/autoXpertWebsite.png",
+        link: "https://www.autoxpert.pl/",
+        color: "#BBACAF"
+    },
+    {
+        title: "Firma Remontowa",
+        description: "lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet",
+        src: "/src/assets/dobrzykWebsite.png",
+        link: "https://dobrzyk.pl/",
+        color: "#BBACAF"
+    },
+
+]
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './projects.css'
+import { projects } from '../Data.js'
+import { Card } from '@mui/material'
 
 const Projects = () => {
   return (
-    <div className='projectsContainer'>Projects</div>
+    <div className='projectsContainer'>
+      {
+        projects.map((project, index) => {
+          <Card key={`p_${index}`} {...project} i={index}/>
+        })
+      }
+    </div>
   )
 }
 
