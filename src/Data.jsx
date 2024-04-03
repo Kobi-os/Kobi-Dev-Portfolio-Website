1e7d4f4b4914d9ab1508dcddf88566d3e5906c30
Wojtek <wojtek.kobier@wp.pl>
2024-04-03 18:27:21 +0200

    projects

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,14 +4,14 @@ export const projects = [
         description: "lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet",
         src: "/src/assets/autoXpertWebsite.png",
         link: "https://www.autoxpert.pl/",
-        color: "white"
+        color: "#F1F1F1 "
     },
     {
         title: "Firma Remontowa",
         description: "lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet lorem ipsum solor dit amet",
         src: "/src/assets/dobrzykWebsite.png",
         link: "https://dobrzyk.pl/",
-        color: "#BBACAF"
+        color: "#E1E9E5 "
     },
 
 ]
