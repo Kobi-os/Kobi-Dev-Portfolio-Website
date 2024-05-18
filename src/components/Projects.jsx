ffadac8aacc4c192d8c9c22cae95432707e3e750
Wojtek <wojtek.kobier@wp.pl>
2024-05-19 00:47:00 +0200

    dsd

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './projects.css'
+import { Typography } from '@mui/material'
 
 const Projects = () => {
   return (
     <div className='projectsContainer' id='Projects'>
       <div className='box'>
         <img className='project-img' src="/src/assets/project logo/photos/autoXpertWebsite.png" alt="" />
+        <div className='placeholder'>
+          <Typography className='project-text'>
+            placeholder text to have something to read
+          </Typography>
+          <button className='project-button'>click me</button>
+        </div>
       </div>
       <div className='box'>
         <img className='project-img' src="/src/assets/project logo/photos/dobrzykWebsite.png" alt="" />
