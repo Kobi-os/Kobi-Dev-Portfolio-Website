27c57505f23283a3e58dc4de7790dd3f206a2397
Wojtek <wojtek.kobier@wp.pl>
2024-05-20 14:45:59 +0200

    asd

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,31 +5,10 @@ import { motion } from "framer-motion";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import MailIcon from '@mui/icons-material/Mail';
+import PermPhoneMsgIcon from '@mui/icons-material/PermPhoneMsg';
 
-const variants = {
-  inital: {
-    y: 500,
-    opacity: 0,
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-};
 
-const contactVariants = [
-  {
-    icon: "Email: ",
-    title: "wojtek.kobier@wp.pl",
-  },
-  {
-    icon: "Phone: ",
-    title: "+48 696 366 110",
-  },
-];
 
 const socials = [
   {
@@ -47,53 +26,34 @@ const socials = [
     icon: <GitHubIcon />,
     link: "https://github.com/Kobi-os"
   },
+  {
+    title: "wojtek.kobier@wp.pl",
+    icon: <MailIcon />,
+  },
+  {
+    title: "696 366 110",
+    icon: <PermPhoneMsgIcon />,
+  },
+  
 ];
 
 const Contact = () => {
   return (
     <div className="contact" id="Contact">
-      <div className="textContainer">
-        {contactVariants.map((item) => (
-          <div className="contactItem">
-            <Typography variant="h4">{item.icon}</Typography>
-            <Typography variant="h5">{item.title}</Typography>
-          </div>
-        ))}
-        <Typography variant="h4" className="contactIntroduction">
-          Where u can find me
-        </Typography>
-      </div>
-      <div className="footerContainer">
-        
-        {socials.map((item) => (
-          <Button
-            color="inherit"
-            className="resumeButton"
-            component={motion.button}
-            initial={{ "--x": "100%", scale: 1 }}
-            animate={{ "--x": "-100%" }}
-            transition={{
-              repeat: Infinity,
-              repeatType: "loop",
-              repeatDelay: 0.1,
-              type: "spring",
-              stiffness: 20,
-              damping: 15,
-              mass: 2,
-              scale: {
-                type: "spring",
-                stiffness: 10,
-                damping: 5,
-                mass: 0.5,
-              },
-            }}
-          >
-            <span className="buttonSpan">{item.icon}</span>
-            <span className="buttonSpan">{item.title}</span>
-            <span className="buttonOverlay" />
-          </Button>
-        ))}
-      </div>
+      <ul className="contact-items">
+          {socials.map((social, key) => (
+            <motion.li
+              key={key}
+              className="social"
+              animate="animate"
+            >
+              <a href={social.link} target="_blank" rel="noreferrer">
+                <Typography variant="h6">{social.title}</Typography>
+                <Icon>{social.icon}</Icon>
+              </a>
+            </motion.li>
+          ))}
+      </ul>
     </div>
   );
 };
